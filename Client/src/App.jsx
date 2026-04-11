2a8479657eb080c1e9ed487ad5dab8bf3fa6e8d8
samanvaya <samayvats@gmail.com>
2026-04-11 20:05:48 +0530

    authentication-done-with-clerk

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,19 +1,43 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import './App.css'
-import Home from './Pages/Home'
-import Auth from './Pages/Auth'
+// App.jsx
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
+import axios from 'axios'
+import { useAuth } from "@clerk/clerk-react";
 
 function App() {
+  const { getToken } = useAuth();
+
+  const checkingTheAuth = async () => {
+    try {
+      const token = await getToken();
+      const result = await axios.post(
+        `http://localhost:8000/api/user/register`,
+        {}, 
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      console.log("result : ", result);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
   return (
     <>
-      <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/auth' element={<Auth/>} />
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
 
-      </Routes>
+      <SignedIn>
+        <h1>Dashboard</h1>
+        <button onClick={checkingTheAuth}>checkin the auth</button>
+        <UserButton />
+      </SignedIn>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- Server/routes/user.route.js
+++ Server/routes/user.route.js
@@ -1,8 +1,10 @@
 import { Router } from "express"
+import {attachUser} from '../middleware/index.js'
 import { register } from "../controllers/user.controller.js"
+import { ClerkExpressRequireAuth } from "@clerk/clerk-sdk-node";
 
 const router = Router()
 
-router.route('/register').post(register)
+router.route('/register').post(ClerkExpressRequireAuth(), attachUser , register)
 
 export default router
